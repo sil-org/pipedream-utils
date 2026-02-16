2014e11a8cc1b9db1937aa44b981621b8455949f
Matt H. <matt_henderson@sil.org>
2026-02-16 12:45:10 -0500

    Split `test.js` file into one file per thing being tested

--- utils/netsuite.test.js
+++ utils/netsuite.test.js
@@ -1,6 +1,6 @@
 import assert from 'node:assert/strict'
 import { describe, it } from 'node:test'
-import { helpers, netsuite } from '../index.js'
+import { netsuite } from '../index.js'
 
 describe('netsuite.getRecordPath', () => {
   it('should get the record path from the URL', () => {
@@ -44,24 +44,5 @@ describe('netsuite.netsuiteRequest', () => {
       config,
     )
     assert.strictEqual(response.statusCode, 204)
-  })
-})
-
-describe('helpers.lastDay', () => {
-  it('should return the beginning of the last day of the current month', () => {
-    const testCases = [
-      { date: new Date('2025-03-01'), expected: new Date('2025-03-31T00:00:00.000Z') },
-      { date: new Date('2025-11-30T07:00:00.0000000-05:00'), expected: new Date('2025-11-30T00:00:00.000Z') },
-      { date: new Date('2025-12-31T23:59:59.999Z'), expected: new Date('2025-12-31T00:00:00.000Z') },
-      { date: new Date('2025-02-28T12:59:10.0000000-05:00'), expected: new Date('2025-02-28T00:00:00.000Z') },
-    ]
-    testCases.forEach(({ date, expected }) => {
-      assert.deepStrictEqual(helpers.lastDay(date), expected)
-    })
-
-    // Test invalid date separately
-    const result = helpers.lastDay('not a date')
-    assert.ok(result instanceof Date)
-    assert.ok(!isNaN(result.getTime()))
   })
 })
---
+++ utils/sftp.test.js
@@ -0,0 +1,3 @@
+import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
+import { sftp } from '../index.js'
